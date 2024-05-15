652fd863b9507c0d09323da289ead4d4cdce779f
quangpao <ddquangbao@gmail.com>
2024-05-15 14:31:31 +0700

    feat: Schedule post features

--- src/modules/automation/commands/button/post-schedule.ts
+++ src/modules/automation/commands/button/post-schedule.ts
@@ -1,19 +1,29 @@
 import type { ButtonCommand } from "@core/types";
-import { ScheduleButton } from "@modules/automation/builder";
+import {
+  ScheduleButton,
+  ScheduleSelectMenuRow,
+} from "@modules/automation/builder";
+import Post from "@modules/prisma/post";
+import { extractEmbed } from "@shared/utils";
+import { EmbedBuilder } from "discord.js";
+import { DateTime } from "luxon";
 
 export const command: ButtonCommand = {
   builder: ScheduleButton,
   async execute(interaction) {
-    const embeds = interaction.message.embeds[0];
-    if (!embeds) {
-      await interaction.reply({
-        content: "Some thing weird happened, please contact the Admin",
-      });
-      return;
-    }
+    const oldEmbed = interaction.message.embeds[0];
+    const extracted = extractEmbed(oldEmbed);
 
-    // return await interaction.update({
-    //   components: [ScheduleSelectMenuRow()],
-    // });
+    await interaction.update({
+      embeds: [EmbedBuilder.from(oldEmbed)],
+    });
+    const post = await Post.getLatest(extracted["id"]);
+    const lastTimestamp =
+      post?.scheduled && post.scheduled > DateTime.now().toSeconds()
+        ? post.scheduled
+        : DateTime.now().toSeconds();
+    return await interaction.editReply({
+      components: [ScheduleSelectMenuRow(lastTimestamp)],
+    });
   },
 };
