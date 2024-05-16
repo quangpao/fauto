9e3dad70dd03affce3d0b5120b921b1deb5c4d16
quangpao <ddquangbao@gmail.com>
2024-05-16 23:17:27 +0700

    feat: Add skip caption button to automation

---
+++ src/modules/automation/commands/button/skip-caption.ts
@@ -0,0 +1,27 @@
+import type { ButtonCommand } from "@core/types";
+import {
+  AddCaptionButton,
+  PostPreviewEmbed,
+  ScheduleRow,
+} from "@modules/automation/builder";
+
+export const command: ButtonCommand = {
+  builder: AddCaptionButton,
+  async execute(interaction) {
+    const oldEmbed = interaction.message?.embeds[0];
+
+    const caption = `Linh ta linh tinh\nCre: Internet\n————————\n#vtmeyes`;
+    const embed = PostPreviewEmbed(
+      {
+        caption,
+        title: "[FAuto] Update post caption",
+      },
+      oldEmbed,
+    );
+
+    return await interaction.update({
+      embeds: [embed],
+      components: [ScheduleRow],
+    });
+  },
+};
